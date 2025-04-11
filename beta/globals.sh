5589e61053db26fdd808f897a786e6722429a66d
KevinGandalf <accounts@homebrew.one>
2025-04-11 20:30:50 +0200

    Create globals.sh

---
+++ beta/globals.sh
@@ -0,0 +1,9 @@
+# globals.sh
+
+# Pfad für ipset-Dateien
+IPSET_FILE_STREAMING="/opt/mpvpn-routing/ipsets/streaming_ips.set"
+IPSET_FILE_GAMING="/opt/mpvpn-routing/ipsets/gaming_ips.set"
+
+# Interface und Routing-Tabelle für Streaming
+stealth_streaming_rtable="500"
+stealth_streaming_if="enp1s0"
